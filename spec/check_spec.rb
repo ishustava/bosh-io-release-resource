b68ae545cd4b33823c9043036f08b14d71e6dcc7
Spencer Hawley <shawley@pivotal.io>
2018-04-25 10:00:24 -0700

    wip: started working on writing tests
    Signed-off-by: Iryna Shustava <ishustava@pivotal.io>

--- spec/check_spec.rb
+++ spec/check_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'json'
 
 RSpec.describe 'check' do
-  def check(repo, version = nil)
+  def check(repo, version = nil, version_regex = nil)
     payload = {
       source: {
         repository: repo
@@ -11,6 +11,10 @@ RSpec.describe 'check' do
 
     if version
       payload[:version] = { version: version }
+    end
+
+    if version_regex
+      payload[:source][:version_regex] = version_regex
     end
 
     output = `echo '#{payload.to_json}' | /opt/resource/check`
@@ -37,13 +41,29 @@ RSpec.describe 'check' do
     end
   end
 
-  context 'when the last know version no longer exists' do
+  context 'when the last known version no longer exists' do
     it 'returns the most recent version' do
       versions = check('concourse/concourse', 'unknown-version')
 
       expect(versions.length).to eq 1
       expect(versions.first).to have_key("version")
       expect(versions.first["version"]).to match /^\d+\.\d+\.\d+$/
+    end
+  end
+
+  context 'when passing a version regex' do
+    context 'when concourse has no previous versions' do
+      it 'produces a list of versions that match specified regex' do
+        versions = check('concourse/concourse', '0.11.0')
+
+        expect(versions.length).to be > 0
+        expect(versions[0]["version"]).to eq '0.11.0'
+        expect(versions[1]["version"]).to eq '0.12.0'
+      end
+    end
+
+    context 'when concourse has a last known version' do
+
     end
   end
 end
